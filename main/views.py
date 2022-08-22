71be5cecdfd1c5d47861cd15a4d939b8acd78747
div1103 <div110393@gmail.com>
2022-08-23 00:48:42 +0300

    form for created tasks

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
@@ -13,5 +14,11 @@ def about(request):
     return render(request, 'about.html')
 
 def create(request):
+    form = TaskForm()
+    context = {
+        'form':form
+    }
     return render(request, 'create.html')
+
+
 # Create your views here.
