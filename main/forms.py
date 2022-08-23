af5f35fbb3e21eedb4fe308aa1f5bb658db3c14c
div1103 <div110393@gmail.com>
2022-08-23 14:56:38 +0300

    form for created tasks

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,12 @@
 from .models import Task
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets, TextInput, DateTimeInput
 
 class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ["title", "task", "deadline", "location"]
+        widgets = {"title": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите название'}),
+                   "task": TextInput(attrs={'class': 'form-control', 'placeholder': 'Введите описание'}),
+                   "deadline": DateTimeInput(attrs={'class': 'form-control', 'placeholder': 'Срок выполнения'}),
+                   "location": TextInput(attrs={'class': 'form-control', 'placeholder': 'Место выполнения'}),
+                   }
